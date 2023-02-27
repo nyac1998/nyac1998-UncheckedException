3405dceab3f6c835dc66ffbf4724066699e8f7ff
Nya Currie <nya1998@gmail.com>
2023-02-27 20:49:02 +0000

    unchecked exception

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,6 +14,7 @@ public class Lab {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
-
+        throw new RuntimeException("This is an unchecked exception!");
     }
+    
 }
